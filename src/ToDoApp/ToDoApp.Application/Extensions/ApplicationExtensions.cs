743a27d5e9d7ce04704efc23432ad2486e233673
ucanbaklava <wexeyeze@gmail.com>
2021-12-19 15:08:56 +0300

    created for registering ToDoService to ioc.

---
+++ src/ToDoApp/ToDoApp.Application/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Application.Interfaces;
+using ToDoApp.Application.Services;
+
+namespace ToDoApp.Application.Extensions
+{
+    public static class ApplicationExtensions
+    {
+        public static IServiceCollection SetupApplication(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IToDoService, ToDoService>();
+            return serviceCollection;
+        }
+    }
+}

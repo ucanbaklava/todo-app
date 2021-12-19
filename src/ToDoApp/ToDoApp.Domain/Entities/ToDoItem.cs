aa5e48398648e7d3345fef9ac7bb02a8103fee6e
ucanbaklava <wexeyeze@gmail.com>
2021-12-19 14:46:21 +0300

    Entity models created.

---
+++ src/ToDoApp/ToDoApp.Domain/Entities/ToDoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp.Domain.Entities
+{
+    public class ToDoItem : BaseEntity
+    {
+        public string TaskContent { get; set; }
+        public bool IsCompleted { get; set; } = false;
+    }
+}

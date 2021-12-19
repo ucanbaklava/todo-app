9e437890a55a63a0baa7e471a388a58fd0363c6f
ucanbaklava <wexeyeze@gmail.com>
2021-12-19 14:51:25 +0300

    ToDoAppContext class created.

---
+++ src/ToDoApp/ToDoApp.Data/Contexts/ToDoAppContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Domain.Entities;
+
+namespace ToDoApp.Data.Contexts
+{
+    public class ToDoAppContext : DbContext
+    {
+        public ToDoAppContext(DbContextOptions<ToDoAppContext> options) : base(options) { }
+
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
